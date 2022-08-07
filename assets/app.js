d22d954f59181370f5f93d768fb91270888d0fd2
mozoou <m.zouheir74@gmail.com>
2022-08-07 10:25:02 +0200

    adding webpack and created react context and rendered the app

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import TodoContextProvider from './js/Context/TodoContext';
+import TodoTable from './js/Components/TodoTable';
+import { createRoot } from 'react-dom/client';
+
+class App extends Component {
+  render() {
+    return  <TodoContextProvider>
+    <TodoTable/>
+    </TodoContextProvider>
+  }
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
