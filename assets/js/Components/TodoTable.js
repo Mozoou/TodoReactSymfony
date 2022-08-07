9cd34aa5c40d62b62a124bf9c1de1dd03d0c7a46
mozoou <m.zouheir74@gmail.com>
2022-08-07 11:18:22 +0200

    added material ui and added CreateTodo feature

--- assets/js/Components/TodoTable.js
+++ assets/js/Components/TodoTable.js
@@ -1,18 +1,59 @@
-import React, { useContext } from "react";
+import {
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import ClearIcon from '@mui/icons-material/Clear';
+import AddIcon from '@mui/icons-material/Add';
+import React, { useContext, useState } from "react";
 import { TodoContext } from "../Context/TodoContext";
 
 function TodoTable() {
-
   const context = useContext(TodoContext);
+  const [addTodo, setAddTodo] = useState('');
 
   return (
-    <div>
-      {context.todos.map(todo => (
-        <div>{todo.task}</div>
-      ))}
-    </div>
+    <form onSubmit={() => {context.createTodo(event,{name:addTodo})}}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Task</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <TextField value={addTodo} onChange={() => {setAddTodo(event.target.value)}} label='New Task' fullWidth={true}/>
+            </TableCell>
+            <TableCell align="right">
+              <IconButton type="submit">
+                <AddIcon></AddIcon>
+              </IconButton>
+            </TableCell>
+          </TableRow>
+          {context.todos.slice().reverse().map((todo, index) => (
+            <TableRow key={'todo' + index}>
+              <TableCell>{todo.name}</TableCell>
+              <TableCell align="right">
+                <IconButton>
+                  <ModeEditIcon></ModeEditIcon>
+                </IconButton>
+                <IconButton>
+                  <ClearIcon></ClearIcon>
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </form>
   );
-
 }
 
 export default TodoTable;
--- assets/js/Context/TodoContext.js
+++ assets/js/Context/TodoContext.js
@@ -3,44 +3,52 @@ import React, { Component, createContext } from "react";
 export const TodoContext = createContext();
 
 export default class TodoContextProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { name: "do something" },
+        { name: "do something" },
+        { name: "do something" },
+        { name: "do something" },
+        { name: "do something" },
+        { name: "do something" },
+      ],
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = { 
-            todos:[{
-                task: 'do something'
-            }],
-        }
-    }
+  // create
+  createTodo(e, todo) {
+    e.preventDefault();
+    console.log(todo)
+    let newTodos = [...this.state.todos];
+    newTodos.push(todo);
+    this.setState({
+        todos:newTodos,
+    })
+  }
+  // read
+  readTodo() {}
+  // update
+  updateTodo() {}
+  // delete
+  deleteTodo() {}
 
-    // create
-    createTodo() {
-
-    }
-    // read
-    readTodo() {
-
-    }
-    // update
-    updateTodo() {
-
-    }
-    // delete
-    deleteTodo() {
-
-    }
-
-    render() {
-        return <div>
-            <TodoContext.Provider value={{
-                ...this.state,
-                createTodo: this.createTodo.bind(this),
-                readTodo: this.readTodo.bind(this),
-                updateTodo: this.updateTodo.bind(this),
-                deleteTodo: this.deleteTodo.bind(this)
-            }}>
-                {this.props.children}
-            </TodoContext.Provider>
-        </div>;
-    }
+  render() {
+    return (
+      <div>
+        <TodoContext.Provider
+          value={{
+            ...this.state,
+            createTodo: this.createTodo.bind(this),
+            readTodo: this.readTodo.bind(this),
+            updateTodo: this.updateTodo.bind(this),
+            deleteTodo: this.deleteTodo.bind(this),
+          }}
+        >
+          {this.props.children}
+        </TodoContext.Provider>
+      </div>
+    );
+  }
 }
